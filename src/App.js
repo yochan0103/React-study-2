054ff13c9e5da4cbe733e62b71df93d5479ad45d
yochan0103 <68014172+yochan0103@users.noreply.github.com>
2022-03-01 18:36:07 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import "./styles.css";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
