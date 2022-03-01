beb428574626470cc84ff2c2879ddaa8e2b6f10a
yochan0103 <68014172+yochan0103@users.noreply.github.com>
2022-03-01 18:41:40 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,8 +17,14 @@ export default function App() {
         <Link to="/page2">Page2</Link>
       </div>
       <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route path="/page1">
           <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
         </Route>
       </Switch>
     </BrowserRouter>
